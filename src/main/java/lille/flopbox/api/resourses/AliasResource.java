81b31c29640a6902197c5352f7ffdd11f9f86633
Hatim M'rabet El Khomssi <mrabet.hatim2018@gmail.com>
2022-03-13 13:52:43 +0100

    Manipulation des alias des serveurs

--- src/main/java/lille/flopbox/api/resourses/AliasResource.java
+++ src/main/java/lille/flopbox/api/resourses/AliasResource.java
@@ -3,10 +3,12 @@ package lille.flopbox.api.resourses;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,6 +24,11 @@ import lille.flopbox.api.auth.Secured;
 @Path("alias")
 public class AliasResource {
 
+    /**
+     * Tous les serveurs d'utilisateur
+     * @param authHeader
+     * @return
+     */
     @GET
     @Secured
     @Produces(MediaType.APPLICATION_JSON)
@@ -31,6 +38,12 @@ public class AliasResource {
         return Response.status(Status.OK).entity(UsersList.getInstance().getServeursByUsername(username)).build();
     }
     
+    /**
+     * avoir le nom du serveur à partir de son alias
+     * @param authHeader
+     * @param alias
+     * @return
+     */
     @Path("{alias}")
     @GET
     @Secured
@@ -39,9 +52,19 @@ public class AliasResource {
     {
         String username = FileManager.getUsernameFromAuth(authHeader);
         User u = UsersList.getInstance().getUserByUsername(username);
-        return Response.status(Status.OK).entity(u.getServeurs().get(alias)).build();
+        if(u.getServeurs().containsKey(alias))
+            return Response.status(Status.OK).entity(u.getServeurs().get(alias)).build();
+        else
+            return Response.status(Status.NOT_FOUND).entity(alias+" alias not found.").build();
     }
 
+    /**
+     * Ajouter un serveur à ma liste des serveur s'il n'existe pas
+     * @param authHeader
+     * @param alias
+     * @param serveur
+     * @return
+     */
     @POST
     @Secured
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -53,6 +76,7 @@ public class AliasResource {
 
         String username = FileManager.getUsernameFromAuth(authHeader);
         User u = UsersList.getInstance().getUserByUsername(username);
+        // verifier l'existance du serveur
         if(u.getServeurs().containsKey(alias))
         {
             JsonObject msg = Json.createObjectBuilder().add("error","alias exist already. To modify it's value, please use PUT.").build();
@@ -62,6 +86,55 @@ public class AliasResource {
         {
             u.addServeur(alias, serveur);
             return Response.status(Status.CREATED).entity(UsersList.getInstance().getServeursByUsername(username)).build();
+        }
+    }
+
+    /**
+     * modifier un alias qui existe deja ou creer un nouveau sinon
+     * @param authHeader
+     * @param alias
+     * @param serveur
+     * @return
+     */
+    @PUT
+    @Secured
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response modifierServeur(@HeaderParam("Authorization") String authHeader, @FormParam("alias") String alias, @FormParam("serveur") String serveur)
+    {
+        if(alias ==  null || serveur == null)
+            return Response.status(Status.BAD_REQUEST).entity(Json.createObjectBuilder().add("error","missing fields.").build()).build();
+
+        String username = FileManager.getUsernameFromAuth(authHeader);
+        User u = UsersList.getInstance().getUserByUsername(username);
+        // verifier l'existance du serveur
+        if(u.getServeurs().containsKey(alias))
+        {
+            u.addServeur(alias, serveur);
+            return Response.status(Status.OK).entity("Modified.").build();
+        }
+        else
+        {
+            u.addServeur(alias, serveur);
+            return Response.status(Status.CREATED).entity(UsersList.getInstance().getServeursByUsername(username)).build();
+        }
+    }
+
+    @DELETE
+    @Path("{alias}")
+    @Secured
+    @Produces(MediaType.TEXT_HTML)
+    public Response modifierServeur(@HeaderParam("Authorization") String authHeader,  @PathParam("alias") String alias)
+    {
+        
+        String username = FileManager.getUsernameFromAuth(authHeader);
+        User u = UsersList.getInstance().getUserByUsername(username);
+        if(u.getServeurs().containsKey(alias))
+        {
+            u.getServeurs().remove(alias);
+            return Response.status(Status.OK).entity("Deleted.").build();
+        } else {
+            return Response.status(Status.NOT_FOUND).entity(alias + " alias not Found.").build();
         }
     }
 }
